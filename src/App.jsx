99101ef3e9451ecdac171fd490856bbea3a1a54b
Rizky Wahyu Hidayat <55789540+riwayatt1@users.noreply.github.com>
2025-02-07 20:27:09 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -37,59 +37,53 @@ const App = () => {
           <div className="flex space-x-8 justify-start">
             <div className="flex flex-col items-center">
               <label className="text-sm text-gray-700 mb-2">Multiple</label>
-              <div className="relative inline-block w-12 mr-2 align-middle select-none transition duration-200 ease-in">
+              <label className="inline-flex items-center cursor-pointer">
                 <input
                   type="checkbox"
+                  value=""
+                  className="sr-only peer"
                   checked={isMultiple}
                   onChange={() => setIsMultiple(!isMultiple)}
-                  className="toggle-checkbox sr-only"
                 />
-                <label
-                  className={`toggle-label block overflow-hidden h-6 rounded-full ${isMultiple ? 'bg-blue-500' : 'bg-gray-300'} cursor-pointer`}
-                >
-                  <span
-                    className={`w-6 h-6 bg-white rounded-full border-4 transition-transform duration-200 ease-in ${isMultiple ? 'translate-x-6' : ''}`}
-                  ></span>
-                </label>
-              </div>
+                <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600 dark:peer-checked:bg-blue-600"></div>
+                <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">
+                  Multiple
+                </span>
+              </label>
             </div>
 
             <div className="flex flex-col items-center">
               <label className="text-sm text-gray-700 mb-2">With Search</label>
-              <div className="relative inline-block w-12 mr-2 align-middle select-none transition duration-200 ease-in">
+              <label className="inline-flex items-center cursor-pointer">
                 <input
                   type="checkbox"
+                  value=""
+                  className="sr-only peer"
                   checked={isWithSearch}
                   onChange={() => setIsWithSearch(!isWithSearch)}
-                  className="toggle-checkbox sr-only"
                 />
-                <label
-                  className={`toggle-label block overflow-hidden h-6 rounded-full ${isWithSearch ? 'bg-blue-500' : 'bg-gray-300'} cursor-pointer`}
-                >
-                  <span
-                    className={`w-6 h-6 bg-white rounded-full border-4 transition-transform duration-200 ease-in ${isWithSearch ? 'translate-x-6' : ''}`}
-                  ></span>
-                </label>
-              </div>
+                <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600 dark:peer-checked:bg-blue-600"></div>
+                <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">
+                  With Search
+                </span>
+              </label>
             </div>
 
             <div className="flex flex-col items-center">
               <label className="text-sm text-gray-700 mb-2">Outlined</label>
-              <div className="relative inline-block w-12 mr-2 align-middle select-none transition duration-200 ease-in">
+              <label className="inline-flex items-center cursor-pointer">
                 <input
                   type="checkbox"
+                  value=""
+                  className="sr-only peer"
                   checked={isOutlined}
                   onChange={() => setIsOutlined(!isOutlined)}
-                  className="toggle-checkbox sr-only"
                 />
-                <label
-                  className={`toggle-label block overflow-hidden h-6 rounded-full ${isOutlined ? 'bg-blue-500' : 'bg-gray-300'} cursor-pointer`}
-                >
-                  <span
-                    className={`w-6 h-6 bg-white rounded-full border-4 transition-transform duration-200 ease-in ${isOutlined ? 'translate-x-6' : ''}`}
-                  ></span>
-                </label>
-              </div>
+                <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600 dark:peer-checked:bg-blue-600"></div>
+                <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">
+                  Outlined
+                </span>
+              </label>
             </div>
           </div>
         </div>
